e67c35555fa0045a011d643f3dba2132964415b3
andresgarria <andresgarria@hotmail.com>
2016-03-27 00:14:48 +0100

    Created Player JUnit test.

---
+++ Game/src/softwareGame/PlayerTest.java
@@ -0,0 +1,49 @@
+package softwareGame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDomino() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddDomino() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveDominoInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveDominoDomino() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNoMoreDominos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchDomino() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchForDouble() {
+		fail("Not yet implemented");
+	}
+
+}
