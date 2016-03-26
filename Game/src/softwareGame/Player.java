9cef099a81b926b398c4fee2c799e62352514064
andresgarria <andresgarria@hotmail.com>
2016-03-27 00:00:59 +0100

    Updating the commenting.

--- Game/src/softwareGame/Player.java
+++ Game/src/softwareGame/Player.java
@@ -3,6 +3,15 @@ package softwareGame;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * CLASS PLAYER
+ * 
+ * @author      Abelardo Valino <abelardo.valino_ovalle@telecom-sudparis.eu>
+ * @author		Andres Gonzalez <andres.gonzalez_arria@telecom-sudparis.eu>
+ * @version     1.0                 (current version number of program)
+ * @since       2016-03-26          (the version of the package this class was first added to)
+ */
+
 public class Player {
 	
 	String name;
