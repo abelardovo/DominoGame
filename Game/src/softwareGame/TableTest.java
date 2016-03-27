64035f72559b7b860b2b7525a3bc5febfb59240e
Abelardo Valiño <abelardojvo@gmail.com>
2016-03-27 21:01:05 +0200

    Javadoc

--- Game/src/softwareGame/TableTest.java
+++ Game/src/softwareGame/TableTest.java
@@ -4,6 +4,14 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * CLASS TABLE TEST
+ * 
+ * @author      Abelardo Valino <abelardo.valino_ovalle@telecom-sudparis.eu>
+ * @author		Andres Gonzalez <andres.gonzalez_arria@telecom-sudparis.eu>
+ * @version     1.0                 (current version number of program)
+ * @since       2016-03-26          (the version of the package this class was first added to)
+ */
 public class TableTest {
 
 	@Test
